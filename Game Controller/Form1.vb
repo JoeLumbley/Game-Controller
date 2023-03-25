765104a00787e0266e855270c473db478b086c14
Joseph Lumbley <joelumbley16@gmail.com>
2023-03-25 14:56:40 -0500

    Added Stick and Trigger Support

--- Game Controller/Form1.vb
+++ Game Controller/Form1.vb
@@ -33,11 +33,11 @@ Public Class Form1
     <StructLayout(LayoutKind.Sequential)> Public Structure JOYINFOEX
         Public dwSize As Integer
         Public dwFlags As Integer
-        Public dwXpos As Integer
-        Public dwYpos As Integer
-        Public dwZpos As Integer 'Xbox: Trigger
-        Public dwRpos As Integer
-        Public dwUpos As Integer
+        Public dwXpos As Integer 'Left Stick: Left / Right
+        Public dwYpos As Integer 'Left Stick: Up / Down
+        Public dwZpos As Integer 'Xbox: Triggers
+        Public dwRpos As Integer 'Right Stick: Up / Down
+        Public dwUpos As Integer 'Right Stick: Left / Right
         Public dwVpos As Integer
         Public dwButtons As Integer
         Public dwButtonNumber As Integer
@@ -53,7 +53,7 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
 
         JI.dwSize = 64
-        JI.dwFlags = &HFF ' All information
+        JI.dwFlags = 255 ' Get all the data.
 
         Timer1.Interval = 32
         Timer1.Start()
@@ -68,7 +68,7 @@ Public Class Form1
 
                 With JI
 
-                    'Buttons
+                    'Buttons***********************************************************************
                     Select Case .dwButtons
                         Case 1
                             '                                         XBox / PlayStation
@@ -87,8 +87,9 @@ Public Class Form1
                             Text = "Joystick: " & CStr(JNum) & " Button: Back / Left Trigger"
                         Case 128
                             Text = "Joystick: " & CStr(JNum) & " Button: Start / Right Trigger"
+                        Case 256
+                            Text = "Joystick: " & CStr(JNum) & " Button: 256"
                     End Select
-
                     If .dwButtons = 0 Then
 
                         Select Case Text
@@ -113,8 +114,12 @@ Public Class Form1
                         End Select
 
                     End If
+                    '******************************************************************************
 
-                    'D-Pad
+                    'D-Pad ************************************************************************
+                    'The range of POV is 0 to 65535.
+                    '0 through 31500 to represent the angle, in degrees.
+                    'degrees = POV \ 100  315° = 31500 \ 100
                     If .dwPOV = 9000 Then
                         Text = "Joystick: " & CStr(JNum) & " D-Pad: Right"
                     End If
@@ -139,7 +144,6 @@ Public Class Form1
                     If .dwPOV = 13500 Then
                         Text = "Joystick: " & CStr(JNum) & " D-Pad: Down Right"
                     End If
-
                     If .dwPOV = 65535 Then
                         If Text = "Joystick: " & CStr(JNum) & " D-Pad: Up" Then
                             Text = ""
@@ -166,69 +170,165 @@ Public Class Form1
                             Text = ""
                         End If
                     End If
+                    '******************************************************************************
 
-                    'Xbox trigger buttons
-                    If (.dwZpos - 32767) / 32768.0 > 0 Then
+                    'Left Stick *******************************************************************
+                    'The range on the y-axis is 0 to 65535.
+                    'Is the left stick in the up position?
+                    If .dwYpos >= 0 Then
+                        If .dwYpos <= 10000 Then
+                            'Yes, the left stick is in the up position.
 
-                        Text = "Joystick: " & CStr(JNum) & " Button: Left Trigger"
+                            Text = "Joystick: " & CStr(JNum) & " Left Stick: Up"
 
+                        End If
                     End If
-                    If (.dwZpos - 32767) / 32768.0 < 0 Then
-
-                        Text = "Joystick: " & CStr(JNum) & " Button: Right Trigger"
-
-                    End If
-
-                    If (.dwZpos - 32767) / 32768.0 = 0 Then
-                        Select Case Text
-                            Case "Joystick: " & CStr(JNum) & " Button: Right Trigger"
-                                Text = ""
-                            Case "Joystick: " & CStr(JNum) & " Button: Left Trigger"
-                                Text = ""
-                        End Select
-                    End If
-
-                    'Xbox left stick
-                    If (.dwYpos - 32767) / 32768.0 = 1 Then
+                    'Is the left stick in the down position?
+                    If .dwYpos >= 55000 Then
+                        'Yes, the left stick is in the down position.
 
                         Text = "Joystick: " & CStr(JNum) & " Left Stick: Down"
 
                     End If
-                    If (.dwYpos - 32767) / 32768.0 < 0 Then
+                    'Has the left stick returned to it neutral position on the y-axis?
+                    If .dwYpos > 10000 Then
+                        If .dwYpos < 55000 Then
+                            'Yes, the left stick has returned to it neutral position y-axis.
 
-                        Text = "Joystick: " & CStr(JNum) & " Left Stick: Up"
+                            Select Case Text
+                                Case "Joystick: " & CStr(JNum) & " Left Stick: Down"
+                                    Text = ""
+                                Case "Joystick: " & CStr(JNum) & " Left Stick: Up"
+                                    Text = ""
+                            End Select
 
+                        End If
                     End If
+                    'The range on the x-axis is 0 to 65535.
+                    'Is the left stick in the left position?
+                    If .dwXpos >= 0 Then
+                        If .dwXpos <= 10000 Then
+                            'Yes, the left stick is in the left position.
 
-                    If CInt((.dwYpos - 32767) / 32768.0) = 0 Then
-                        Select Case Text
-                            Case "Joystick: " & CStr(JNum) & " Left Stick: Down"
-                                Text = ""
-                            Case "Joystick: " & CStr(JNum) & " Left Stick: Up"
-                                Text = ""
-                        End Select
+                            Text = "Joystick: " & CStr(JNum) & " Left Stick: Left"
+                        End If
                     End If
-
-                    If (.dwXpos - 32767) / 32768.0 > 0 Then
+                    'Is the left stick in the right position?
+                    If .dwXpos >= 55000 Then
+                        'Yes, the left stick is in the right position.
 
                         Text = "Joystick: " & CStr(JNum) & " Left Stick: Right"
+                    End If
+                    'Has the left stick returned to it neutral position on the x-axis?
+                    If .dwXpos > 10000 Then
+                        If .dwXpos < 55000 Then
+                            'Yes, the left stick has returned to it neutral position on the x-axis.
+
+                            Select Case Text
+                                Case "Joystick: " & CStr(JNum) & " Left Stick: Left"
+                                    Text = ""
+                                Case "Joystick: " & CStr(JNum) & " Left Stick: Right"
+                                    Text = ""
+                            End Select
+
+                        End If
+                    End If
+                    '******************************************************************************
+
+                    'Right Stick *******************************************************************
+                    'The range on the r-axis is 0 to 65535.
+                    'Is the right stick in the up position?
+                    If .dwRpos >= 0 Then
+                        If .dwRpos <= 10000 Then
+                            'Yes, the right stick is in the up position.
+
+                            Text = "Joystick: " & CStr(JNum) & " Right Stick: Up"
+
+                        End If
+                    End If
+                    'Is the right stick in the down position?
+                    If .dwRpos >= 55000 Then
+                        'Yes, the right stick is in the down position.
+
+                        Text = "Joystick: " & CStr(JNum) & " Right Stick: Down"
 
                     End If
-                    If (.dwXpos - 32767) / 32768.0 < 0 Then
+                    'Has the right stick returned to it neutral position on the y-axis?
+                    If .dwRpos > 10000 Then
+                        If .dwRpos < 55000 Then
+                            'Yes, the right stick has returned to it neutral position y-axis.
 
-                        Text = "Joystick: " & CStr(JNum) & " Left Stick: Left"
+                            Select Case Text
+                                Case "Joystick: " & CStr(JNum) & " Right Stick: Down"
+                                    Text = ""
+                                Case "Joystick: " & CStr(JNum) & " Right Stick: Up"
+                                    Text = ""
+                            End Select
 
+                        End If
                     End If
+                    'The range on the u-axis is 0 to 65535.
+                    'Is the right stick in the left position?
+                    If .dwUpos >= 0 Then
+                        If .dwUpos <= 10000 Then
+                            'Yes, the right stick is in the left position.
 
-                    If CInt((.dwXpos - 32767) / 32768.0) = 0 Then
-                        Select Case Text
-                            Case "Joystick: " & CStr(JNum) & " Left Stick: Right"
-                                Text = ""
-                            Case "Joystick: " & CStr(JNum) & " Left Stick: Left"
-                                Text = ""
-                        End Select
+                            Text = "Joystick: " & CStr(JNum) & " Right Stick: Left"
+                        End If
                     End If
+                    'Is the right stick in the right position?
+                    If .dwUpos >= 55000 Then
+                        'Yes, the right stick is in the right position.
 
+                        Text = "Joystick: " & CStr(JNum) & " Right Stick: Right"
+                    End If
+                    'Has the right stick returned to it neutral position on the x-axis?
+                    If .dwUpos > 10000 Then
+                        If .dwUpos < 55000 Then
+                            'Yes, the right stick has returned to it neutral position on the x-axis.
+
+                            Select Case Text
+                                Case "Joystick: " & CStr(JNum) & " Right Stick: Left"
+                                    Text = ""
+                                Case "Joystick: " & CStr(JNum) & " Right Stick: Right"
+                                    Text = ""
+                            End Select
+
+                        End If
+                    End If
+                    '******************************************************************************
+
+                    'Triggers**********************************************************************
+                    'The range on the z-axis is 0 to 65535.
+                    'Is the right stick in the left position?
+                    If .dwZpos >= 0 Then
+                        If .dwZpos <= 10000 Then
+                            'Yes, the right stick is in the left position.
+
+                            Text = "Joystick: " & CStr(JNum) & " Right Trigger"
+                        End If
+                    End If
+                    'Is the right stick in the right position?
+                    If .dwZpos >= 55000 Then
+                        'Yes, the right stick is in the right position.
+
+                        Text = "Joystick: " & CStr(JNum) & " Left Trigger"
+                    End If
+                    'Has the right stick returned to it neutral position on the x-axis?
+                    If .dwZpos > 10000 Then
+                        If .dwZpos < 55000 Then
+                            'Yes, the right stick has returned to it neutral position on the x-axis.
+
+                            Select Case Text
+                                Case "Joystick: " & CStr(JNum) & " Left Trigger"
+                                    Text = ""
+                                Case "Joystick: " & CStr(JNum) & " Right Trigger"
+                                    Text = ""
+                            End Select
+
+                        End If
+                    End If
+                    '******************************************************************************
 
                 End With
 
